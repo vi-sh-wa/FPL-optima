32705526777f376e12b17c088b119e410d02bf51
Vishwa <vishwa98.co@gmail.com>
2026-02-23 21:17:22 +0000

    Synced fpl columns and past data columns

--- weekly_fpl.py
+++ weekly_fpl.py
@@ -10,10 +10,17 @@ client = bigquery.Client()
 TABLE_ID = "fpl-optima.fpl_bronze.fpl"
 
 def main():
+
+    boot_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    boot_data = requests.get(boot_url).json()
+    players_meta = pd.DataFrame(boot_data['elements'])[['id', 'first_name', 'second_name']]
+    players_meta['full_name'] = players_meta['first_name'] + "_" + players_meta['second_name']
+    name_map = dict(zip(players_meta['id'], players_meta['full_name']))
+
     query = "SELECT DISTINCT id FROM `fpl-optima.fpl_bronze.current_epl_players`"
     active_ids = client.query(query).to_dataframe()['id'].tolist()
 
-    last_gw_query = "SELECT MAX(gw) as last_gw FROM `fpl-optima.fpl_bronze.fpl`"
+    last_gw_query = "SELECT MAX(gw) as last_gw FROM `fpl-optima.fpl_bronze.fpl` where season = '2025-26'"
     last_gw = client.query(last_gw_query).to_dataframe()['last_gw'].iloc[0]
 
     # If empty (first time running), start from 0
@@ -30,6 +37,8 @@ def main():
                         data = r.json()
                         if 'history' in data and data['history']:
                             df = pd.DataFrame(data['history'])
+                            df['name'] = name_map.get(p_id) 
+                            df['season'] = '2025/26'
                             
                             if 'round' in df.columns:
                                 df['gw'] = df['round'] 
