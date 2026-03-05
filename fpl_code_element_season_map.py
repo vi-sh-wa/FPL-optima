d137df98c6b5ec76e911b335c887102a92d13f36
Vishwa <vishwa98.co@gmail.com>
2026-03-05 20:15:27 +0000

    fixed errors

--- fpl_code_element_season_map.py
+++ fpl_code_element_season_map.py
@@ -6,7 +6,6 @@ client = bigquery.Client()
 table_id = "fpl-optima.fpl_bronze.fpl_code_element_season"
 
 def map_id():
-    import pandas as pd
     base_url = "https://raw.githubusercontent.com/ChrisMusson/FPL-ID-Map/main/FPL/"
 
     seasons = [
@@ -36,7 +35,7 @@ def map_id():
     fpl_code_element_season_map = pd.concat(all_dfs, ignore_index=True)
 
     print(f"Uploading {len(fpl_code_element_season_map)} player mappings to BigQuery...")
-    
+
     job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
     job = client.load_table_from_dataframe(fpl_code_element_season_map, table_id, job_config=job_config)
     
