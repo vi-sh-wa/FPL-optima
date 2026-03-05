50c944567e79c26a2776b950387a7ade6f8ac1cb
Vishwa <vishwa98.co@gmail.com>
2026-03-05 20:21:13 +0000

    Ingesting understat x fpl player code map

---
+++ fpl_code_understat_id_map.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from google.cloud import bigquery
+
+client = bigquery.Client()
+
+table_id = "fpl-optima.fpl_bronze.fpl_code_understat"
+
+def code_x_understat_map_id():
+    url = "https://github.com/ChrisMusson/FPL-ID-Map/blob/main/Understat.csv"
+    df = pd.read_csv(url)
+
+    print(f"Uploading {len(df)} player mappings to BigQuery...")
+
+    job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
+    
+    job.result() 
+
+    print(f"Mission complete")
+
+if __name__ == "__main__":
+    code_x_understat_map_id()
