f382a3ef486c162517bece41304dd7fbb234e030
Vishwa <vishwa98.co@gmail.com>
2026-03-08 21:00:34 +0000

    Updated code

--- get_roster.py
+++ get_roster.py
@@ -37,9 +37,12 @@ def get_roster():
                 opp_side = 'a' if side == 'h' else 'h'
                 
                 for p_id, stats in team_data.items():
+                    stats['roster_id'] = stats.pop('id')
                     stats['match_id'] = m_id
                     stats['team_name'] = match_map[m_id][side]
                     stats['opponent_name'] = match_map[m_id][opp_side]
+                    stats['datetime'] = match_map[m_id]['datetime']
+                    stats['season'] = match_map[m_id]['season']
                     player_match_data.append(stats)
             
             time.sleep(random.uniform(0.6, 1.2))
@@ -52,16 +55,14 @@ def get_roster():
 
         df['match_id'] = df['match_id'].astype(str)
         
-        # Quick numeric cleanup
         for col in ['xG', 'xA', 'npxG', 'xGChain', 'xGBuildup']:
             if col in df.columns:
                 df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0.0)
 
         # Save to the "data" folder
         file_path = os.path.join(os.getcwd(), "data", "EPL_player_match_stats.parquet")
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
         df.to_parquet(file_path, index=False)
-        print(f"Done! Saved to {file_path}")
+        print(f"Yoo! It's done..Go on, do your analysis. File saved at: {file_path}")
 
 
 if __name__ == "__main__":
