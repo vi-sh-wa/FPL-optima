67abe3d7bb79a18c533894a5a779804381957b79
Vishwa <vishwa98.co@gmail.com>
2026-03-09 14:43:05 +0000

    Created a seperate folder for ingestion scripts

---
+++ ingestion_scripts/get_match.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import os
+from understatapi import UnderstatClient
+understat = UnderstatClient()
+
+file_path = "data/EPL_match_summary.parquet"
+
+if os.path.exists(file_path):
+    existing_data = pd.read_parquet(file_path, columns=['match_id'])
+    completed_macthes = set(existing_data['match_id'].astype(str).tolist())
+    print('Ingesting this weeks data...')
+else:
+    completed_macthes = set()
+    print('Starting fresh...')
+
+seasons = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
+
+macth_list = []
+
+for season in seasons:
+    data = understat.league(league="EPL").get_match_data(season=season)
+    for matches in data:
+        m_id = str(matches['id'])
+
+        if str(matches['isResult']).lower() == 'true' and m_id not in completed_macthes:
+            macth_list.append({
+                'match_id': m_id,
+                'home_team': matches['h']['title'],
+                'away_team': matches['a']['title'],
+                'h_goals': int(matches['goals']['h']),
+                'a_goals': int(matches['goals']['a']),
+                'h_xG': float(matches['xG']['h']),
+                'a_xG': float(matches['xG']['a']),
+                'datetime': matches['datetime'],
+                'h_win_forecast': float(matches['forecast']['w']),
+                'h_draw_forecast': float(matches['forecast']['d']),
+                'h_win_forecast': float(matches['forecast']['l']),
+            })
+
+if macth_list:
+    epl_matches = pd.DataFrame(macth_list)
+    
+    if not completed_macthes:
+        updated_match_info = epl_matches
+    else:
+        existing_match_info = pd.read_parquet(file_path)
+        updated_match_info = pd.concat([existing_match_info, epl_matches], ignore_index=True)
+    
+    updated_match_info.to_parquet(file_path, index=False)
+    print(f"Added {len(macth_list)} new matches. Total matches: {len(updated_match_info)}")
+else:
+    print("No new matches found since last update.")
---
+++ ingestion_scripts/get_roster.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import os
+import time
+import random
+from tqdm import tqdm
+from understatapi import UnderstatClient
+understat = UnderstatClient()
+
+seasons = [2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]
+match_map = {}
+
+def get_roster():
+    for season in seasons:
+        print(f"Mapping season: {season}")
+        league_matches = understat.league(league="EPL").get_match_data(season=season)
+        
+        for m in league_matches:
+            m_id = m['id']
+            if str(m['isResult']).lower() == 'true':
+                match_map[m_id] = {
+                    'h': m['h']['title'],
+                    'a': m['a']['title'],
+                    'season': season,
+                    'datetime': m['datetime'] 
+                }
+
+    match_ids = list(match_map.keys())
+    print(f"Total matches to scrape: {len(match_ids)}")
+
+    player_match_data = []
+
+    for m_id in tqdm(match_ids, desc="Scraping Roster Details"):
+        try:
+            roster = understat.match(match=m_id).get_roster_data()
+            
+            for side in ['h', 'a']:
+                team_data = roster.get(side, {})
+                opp_side = 'a' if side == 'h' else 'h'
+                
+                for p_id, stats in team_data.items():
+                    stats['roster_id'] = stats.pop('id')
+                    stats['match_id'] = m_id
+                    stats['team_name'] = match_map[m_id][side]
+                    stats['opponent_name'] = match_map[m_id][opp_side]
+                    stats['datetime'] = match_map[m_id]['datetime']
+                    stats['season'] = match_map[m_id]['season']
+                    player_match_data.append(stats)
+            
+            time.sleep(random.uniform(0.6, 1.2))
+            
+        except Exception as e:
+            print(f"Error on match {m_id}: {e}")
+
+    if player_match_data:
+        df = pd.DataFrame(player_match_data)
+
+        df['match_id'] = df['match_id'].astype(str)
+        
+        for col in ['xG', 'xA', 'npxG', 'xGChain', 'xGBuildup']:
+            if col in df.columns:
+                df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0.0)
+
+        # Save to the "data" folder
+        file_path = os.path.join(os.getcwd(), "data", "EPL_player_match_stats.parquet")
+        df.to_parquet(file_path, index=False)
+        print(f"Yoo! It's done..Go on, do your analysis. File saved at: {file_path}")
+
+
+if __name__ == "__main__":
+    get_roster()
