492964bf9939d18f3b1e052e1fcd53f8d3764ee7
Vishwa <vishwa98.co@gmail.com>
2026-03-19 16:11:59 +0000

    Added code for weekly fixture table update and fixed mid season team name change error

--- ingestion_scripts/fpl_scrapper.py
+++ ingestion_scripts/fpl_scrapper.py
@@ -2,6 +2,7 @@ import requests
 import time
 from tqdm import tqdm
 import pandas as pd
+import os
 
 def get_fpl_metadata():
     boot_url = "https://fantasy.premierleague.com/api/bootstrap-static/"
@@ -9,6 +10,7 @@ def get_fpl_metadata():
     if r.status_code != 200:
         raise Exception("Failed to reach FPL API")
     return r.json()
+
 
 def get_player_history(player_map, season_label, rounds_to_fetch):
     all_new_data = []
@@ -45,4 +47,78 @@ def get_player_mappings(boot_data):
             'code': str(p['code']) 
         } for p in boot_data['elements']
     }
-    return player_map
+    return player_map, team_map
+
+
+def sync_fpl_fixtures(team_map):
+    fixture_url = "https://fantasy.premierleague.com/api/fixtures/"
+    print("Fetching fixtures from FPL API...")
+    response = requests.get(fixture_url)
+    fixtures_data = response.json()
+
+    clean_fixtures = []
+    fix_id_map = {}
+
+    for fixture in fixtures_data:
+        h_id = fixture['team_h']
+        a_id = fixture['team_a']
+        fix_id_map[fixture['id']] = (h_id, a_id)
+
+        if not fixture.get('stats'):
+            continue
+            
+        h_name = team_map.get(h_id, "Unknown")
+        a_name = team_map.get(a_id, "Unknown")
+
+        for stat_item in fixture['stats']:
+            if stat_item['identifier'] == 'bps':
+                away_bps_total = sum(player['value'] for player in stat_item["a"])
+                home_bps_total = sum(player['value'] for player in stat_item["h"])
+
+                clean_fixtures.append({
+                    'fixture_id': fixture['id'],
+                    'round': fixture['event'],
+                    'home_team': h_name,
+                    'away_team': a_name,
+                    'h_bps': home_bps_total,
+                    'a_bps': away_bps_total,
+                    'h_score': fixture['team_h_score'],
+                    'a_score': fixture['team_a_score'],
+                    'kickoff_time': fixture['kickoff_time'],
+                    'finished' : fixture['finished'],
+                })
+                break
+    
+    df = pd.DataFrame(clean_fixtures)
+    if not df.empty and 'kickoff_time' in df.columns:
+        df['kickoff_time'] = pd.to_datetime(df['kickoff_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    return df, fix_id_map
+
+def update_fixture_table(df_new, file_path):
+    if os.path.exists(file_path):
+        df_old = pd.read_parquet(file_path)
+        df_combined = pd.concat([df_old, df_new], ignore_index=True)
+        df_combined = df_combined.drop_duplicates(subset=['fixture_id'], keep='last')
+    else:
+        df_combined = df_new
+
+    df_combined.to_parquet(file_path, index=False)
+    print(f"Fixture table updated at {file_path}")
+
+
+def fix_player_teams(df, fix_id_map, team_map):
+    def resolve_team(row):
+        h_id, a_id = fix_id_map.get(row['fixture'], (None, None))
+        p_team_id = h_id if row['was_home'] else a_id
+        opp_team_id = a_id if row['was_home'] else h_id
+        
+        return pd.Series([
+            team_map.get(p_team_id, "Unknown"), 
+            team_map.get(opp_team_id, "Unknown")
+        ])
+
+    df[['team', 'opponent_team']] = df.apply(resolve_team, axis=1)
+    return df
+
+
--- ingestion_scripts/weekly_sync.py
+++ ingestion_scripts/weekly_sync.py
@@ -15,38 +15,54 @@ USTAT_SEASONS = config['current_season_understat']
 FPL_PATH = config['paths']['fpl_current']
 USTAT_SUMMARY_PATH = config['paths']['understat_summary']
 USTAT_ROSTER_PATH = config['paths']['understat_roster']
-
+FPL_FIXTURE = config['paths']['fpl_fixture']
 
 
 def run_fpl_pipeline():
-    print(f"\n--- Starting FPL Update for {FPL_SEASON} ---")
-    
-    #Checking local file
+    print(f"\n Initializing pipeline... ")
+                                                                                                                # Checking if files exists
+    if os.path.exists(FPL_FIXTURE):
+        print(f"FPL fixture data exists.... ")
+        existing_fixtures = pd.read_parquet(FPL_FIXTURE)
+        last_fixture_round = existing_fixtures['round'].max() if not existing_fixtures.empty else 0
+    else:
+        print(f"Current season fixture data empty. Populating data from API... ")
+        last_fixture_round = 0
+
     if os.path.exists(FPL_PATH):
+        print(f"FPL player data exists.... ")
         existing_df = pd.read_parquet(FPL_PATH)
         season_data = existing_df[existing_df['season'] == FPL_SEASON]
         last_saved_round = season_data['round'].max() if not season_data.empty else 0
     else:
+        print(f"Current season player data empty. Populating data from API... ")
         existing_df = pd.DataFrame()
         last_saved_round = 0
 
-    # Checking if/which GW is ready from the api
-    boot_data = get_fpl_metadata()
-    # Problem Fix: Correctly identifying finished/checked rounds
+                                                                                                                # Comparing rounds from api and local
+    boot_data = get_fpl_metadata()                                                                         
+
     ready_rounds = [e['id'] for e in boot_data['events'] if e['finished'] and e['data_checked']]
     max_ready_round = max(ready_rounds) if ready_rounds else 0
 
     rounds_to_fetch = list(range(int(last_saved_round + 1), int(max_ready_round + 1)))
-    if not rounds_to_fetch:
-        print(f"Data is already up to date (GW {last_saved_round}).")
+    fixtures_need_update = max_ready_round > last_fixture_round
+                                                                                                                # Decision 
+    if not rounds_to_fetch and not fixtures_need_update:
+        print(f"\n All tables are up to date (GW {max_ready_round}).")
         return
-    print(f"Missing gameweek(s): {rounds_to_fetch}")
 
+    player_map, team_map = get_player_mappings(boot_data)
+    new_fpl_fixtures, fix_id_map = sync_fpl_fixtures(team_map)
 
-    player_map = get_player_mappings(boot_data)
-    new_data_df = get_player_history(player_map, FPL_SEASON, rounds_to_fetch)
-
-    if not new_data_df.empty:
+    if fixtures_need_update:
+        print(f"\n Updating FPL fixture table to GW {max_ready_round}...")
+        update_fixture_table(new_fpl_fixtures, FPL_FIXTURE)
+                                                                                                                # Ingesting data
+    if rounds_to_fetch:
+        print(f"\n Updating FPL player table to GW {max_ready_round}...")
+        new_data_df = get_player_history(player_map, FPL_SEASON, rounds_to_fetch)
+        new_data_df = fix_player_teams(new_data_df, fix_id_map, team_map)
         new_data_df['kickoff_time'] = pd.to_datetime(new_data_df['kickoff_time']).dt.strftime('%Y-%m-%d %H:%M:%S')
         combined_df = pd.concat([existing_df, new_data_df], ignore_index=True)
 
@@ -58,11 +74,7 @@ def run_fpl_pipeline():
                 combined_df[col] = pd.to_numeric(combined_df[col], errors='coerce').fillna(0.0)
 
         combined_df = combined_df.drop_duplicates(subset=['name', 'round', 'fixture', 'season'], keep='last')
-        
         combined_df.to_parquet(FPL_PATH, index=False)
-        print(f"Success: Gameweek {rounds_to_fetch} added to current seasons data.")
-
-
 
 
 
